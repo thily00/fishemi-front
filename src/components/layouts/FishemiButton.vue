a291aec00c119e41f483f0b9eb729f0278f78840
dorian <dorian.vidal@hetic.net>
2024-06-14 11:19:58 +0200

    button

---
+++ src/components/layouts/FishemiButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Button from "primevue/button";
+
+const props = defineProps({
+  icon: String,
+  label: String,
+  action: Function,
+});
+</script>
+<template>
+  <div class="card flex justify-center">
+    <Button :icon="props.icon" :label="props.label" @click="props.action" />
+  </div>
+</template>
