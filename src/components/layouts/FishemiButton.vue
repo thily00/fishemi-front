9fcad2e7ed850a33cc599559dbd977d588adde8e
dorian <dorian.vidal@hetic.net>
2024-06-14 12:01:33 +0200

    buttons style

--- src/components/layouts/FishemiButton.vue
+++ src/components/layouts/FishemiButton.vue
@@ -7,8 +7,31 @@ const props = defineProps({
   action: Function,
 });
 </script>
+
 <template>
   <div class="card flex justify-center">
-    <Button :icon="props.icon" :label="props.label" @click="props.action" />
+    <Button
+      :icon="props.icon"
+      :label="props.label"
+      @click="props.action"
+      class="custom-button"
+    />
   </div>
 </template>
+
+<style>
+.custom-button {
+  background: radial-gradient(circle, #e9ff90 0%, #ccff00 40%);
+  border: none;
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 12px 25px;
+  border-radius: 8px;
+  transition: background 0.3s ease;
+}
+.custom-button > span {
+  font-family: "Aeonik", sans-serif !important;
+  font-weight: 500 !important;
+}
+</style>
