24517e619cf840a3bd02acfaadfed82ef6886336
Djibril Bathily <bathilydbl@gmail.com>
2024-06-14 10:38:11 +0200

    install prime vue and tailwind css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Lara from '@/presets/lara'
+
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    unstyled: true,
+    pt: Lara 
+});
 
 app.mount('#app')
---
+++ src/presets/lara/index.d.ts
@@ -0,0 +1,4 @@
+declare module '@/presets/lara' {
+    const Lara: any; // Replace 'any' with the actual type of Lara
+    export default Lara;
+  }
