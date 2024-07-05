94d2b10e79924a25d849b12be8e3c5b7b58ea76d
dorian <dorian.vidal@hetic.net>
2024-07-05 09:30:19 +0200

    add dashboard page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/Auth/LoginView.vue')
-    }
-  ]
-})
+      path: "/login",
+      name: "login",
+      component: () => import("../views/Auth/LoginView.vue"),
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: () => import("../views/app/Dashboard.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/app/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>ilies gay</h1>
+  </div>
+</template>
+
+<script setup>
+import InputText from "primevue/inputtext";
+import FishemiButton from "@/components/layouts/FishemiButton.vue";
+import FishemiContainer from "@/components/layouts/FishemiContainer.vue";
+</script>
+
+<style scoped></style>
