d60a6a42eb1eb4328ed95d3943bfec35a96d9843
Djibril Bathily <bathilydbl@gmail.com>
2024-07-11 18:58:54 +0200

    preprod deployment

--- src/router/index.js
+++ src/router/index.js
@@ -6,27 +6,27 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: () => import("../views/HomeView.vue"),
+      component: () => import("@/views/HomeView.vue"),
     },
     {
       path: "/login",
       name: "login",
-      component: () => import("../views/auth/LoginView.vue"),
+      component: () => import("@/views/auth/LoginView.vue"),
     },
     {
       path: "/register",
       name: "register",
-      component: () => import("../views/auth/RegisterView.vue"),
+      component: () => import("@/views/auth/RegisterView.vue"),
     },
     {
       path: "/dashboard",
       name: "dashboard",
-      component: () => import("../views/app/Dashboard.vue"),
+      component: () => import("@/views/app/Dashboard.vue"),
     },
     {
       path: "/mes-employes",
       name: "employees",
-      component: () => import("../views/app/EmployeesView.vue"),
+      component: () => import("@/views/app/EmployeesView.vue"),
     }  
   ],
 });
