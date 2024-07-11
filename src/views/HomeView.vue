9680013d229b0070a50e5de8860d7aae231b050d
Djibril Bathily <bathilydbl@gmail.com>
2024-07-11 20:09:57 +0200

    preprod deployment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HeroSection from '../components/home/HeroSection.vue';
+import HeroSection from '@/components/home/HeroSection.vue';
 import FishemiContainer from '@/components/layouts/FishemiContainer.vue';
 </script>
 
