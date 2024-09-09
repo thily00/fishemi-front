b4dc54c4837c9b6304a50c1a3dd4bbf3c1e9816e
Djibril Bathily <bathilydbl@gmail.com>
2024-09-09 14:25:42 +0200

    fix types

--- src/components/layouts/FishemiModal.vue
+++ src/components/layouts/FishemiModal.vue
@@ -1,49 +1,48 @@
 <script setup>
-import { defineEmits } from 'vue';
+import { defineEmits } from "vue";
 
-const emit = defineEmits(['close']);
+const emit = defineEmits(["close"]);
 const props = defineProps({
   isVisible: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 });
 
 const close = () => {
-  emit('close');
-}
+  emit("close");
+};
 </script>
 <template>
-    <div v-if="props.isVisible" class="modal-overlay" @click.self="close">
-      <div class="modal-content">
-        <slot></slot>
-        <div class="absolute top-8 right-8 cursor-pointer" @click="close" >
-            <i class="pi pi-times text-white"></i>
-        </div>
-        <!-- <button @click="close">Fermer</button> -->
+  <div v-if="props.isVisible" class="modal-overlay" @click.self="close">
+    <div class="modal-content">
+      <slot></slot>
+      <div class="absolute top-8 right-8 cursor-pointer" @click="close">
+        <i class="pi pi-times text-white"></i>
       </div>
+      <!-- <button @click="close">Fermer</button> -->
     </div>
-</template> 
+  </div>
+</template>
 <style scoped>
-  .modal-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .modal-content {
-    position: relative;
-    background: var(--blue);
-    padding: 20px;
-    border-radius: 5px;
-    width: 100%;
-    max-width: 400px;
-  }
-  </style>
-  
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  position: relative;
+  background: var(--blue);
+  padding: 20px;
+  border-radius: 5px;
+  width: 100%;
+  max-width: 400px;
+}
+</style>
